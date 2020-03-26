2342de2bc909796fbd74b9c406ae8cd8af0dc0c1
KISLAYA AAKASH <kislaya.aakash@publicissapient.com>
2020-03-26 21:43:55 +0530

    everything is final

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -5,33 +5,7 @@ import ExpiryDate from "../ExpiryDate";
 import Branding from "../Branding/branding";
 
 export class CourseCard extends Component {
-    render() {
-        return (
-            <div  className="col-sm-4 card course-card" >
-                <div className="row" style={{marginTop:"35px"}}>
-
-                        <div className="col-sm-6 course-img">
-                            <CourseImg percent={this.props.data.Percent} image={this.props.data["Img"]}/>
-                        </div>
-
-                        <div className="col-sm-6 course-details col">
-                            <>
-                                <p>{this.props.data["Name"]}</p> 
-                                {/* <p>Lorem ipsum dolor sit, amet </p> */}
-                                {
-                                (this.props.data.Percent===100) ? <p>Course Completed !!</p> // do the changes here
-                                :
-                                <div>
-                                    <ExpiryDate sdate={this.props.data["SDate"]}> </ExpiryDate>
-                                    <ExpiryDate edate={this.props.data["EDate"]}/>
-                                    </div> } 
-                                
-                            </>
-                        </div>
-                </div>
-            </div>
-        )
-    }
+    
 	render() {
 		return (
 			<div className="col-md-4 card course-card">
@@ -52,7 +26,7 @@ export class CourseCard extends Component {
 								<p>
 									Completed!!
 									<br />
-									<Branding />
+									<Branding userId = "143" course = "Java"/>
 								</p>
 							) : (
 								<div>
